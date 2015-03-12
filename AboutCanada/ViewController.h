ab78003b108b640119c4c03442da1a759da28eba
Asfanur <asfanur.arafin@gmail.com>
2015-03-12 17:03:17 +1100

    Initial Commit

---
+++ AboutCanada/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AboutCanada
+//
+//  Created by Asfanur Arafin on 12/03/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AboutCanadaTests/AboutCanadaTests.m
@@ -0,0 +1,40 @@
+//
+//  AboutCanadaTests.m
+//  AboutCanadaTests
+//
+//  Created by Asfanur Arafin on 12/03/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AboutCanadaTests : XCTestCase
+
+@end
+
+@implementation AboutCanadaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
