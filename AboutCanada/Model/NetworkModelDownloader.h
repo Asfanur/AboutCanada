cf0f4fc0351a969d871b9ed9c4ccfa62ec763a27
Asfanur <asfanur.arafin@gmail.com>
2015-03-12 17:12:27 +1100

    initial commit, Added NetworkModelDownloader class To download the JSON

--- AboutCanada/Model/NetworkModelDownloader.h
+++ AboutCanada/Model/NetworkModelDownloader.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  NetworkModelDownloader.h
 //  AboutCanada
 //
 //  Created by Asfanur Arafin on 12/03/2015.
 //  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
-
+@interface NetworkModelDownloader : NSObject
 
 @end
-
---
+++ AboutCanada/Model/NetworkModelDownloader.m
@@ -0,0 +1,13 @@
+//
+//  NetworkModelDownloader.m
+//  AboutCanada
+//
+//  Created by Asfanur Arafin on 12/03/2015.
+//  Copyright (c) 2015 Asfanur Arafin. All rights reserved.
+//
+
+#import "NetworkModelDownloader.h"
+
+@implementation NetworkModelDownloader
+
+@end
